6fe8590537ea8613051445890af0f7f2ace44b66
kyle.wilson <kyle.wilson@foxmail.com>
2022-08-08 17:00:42 +0800

    adjust

--- prim/main.cpp
+++ prim/main.cpp
@@ -1,67 +1,75 @@
 #include <iostream>
 
-using namespace std;
-
-#define MAXN 100
-int n, m, temp, ans = 0, map[MAXN][MAXN], length[MAXN];
-char s, t;
-bool flag[MAXN];
+#define MAX_N 100
+int n, map[MAX_N][MAX_N];
 
 void init() {
 
-    cin >> n >> m;
-    memset(map, -1, MAXN * MAXN);
+    memset(map, 0x3f, MAX_N * MAX_N);
     for (int i = 0; i < n; ++i) {
         map[i][i] = 0;
-        flag[i] = false;
-        length[i] = 0x7fffffff;
     }
 
+    int m, temp;
+    char s, t;
+
+    scanf("%d %d", &n, &m);
     for (int i = 0; i < m; ++i) {
-        cin >> s >> t >> temp;
+        scanf(" %c %c %d", &s, &t, &temp);
         map[s - 'A'][t - 'A'] = temp;
         map[t - 'A'][s - 'A'] = temp;
     }
 
 }
 
-int main() {
+int prim() {
 
-    freopen("../a.in", "r", stdin);
-    init();
-
+    int ans = 0, dist[MAX_N];
+    bool visit[MAX_N] = {false};
     // 将0作为起点加入集合S
-    flag[0] = true;
-    for (int i = 0; i < n; ++i) {
-        if (map[0][i] >= 0) {
-            length[i] = map[0][i];
-        }
-    }
+    visit[0] = true;
+
+    // dist初始化
+    memset(dist, 0x3f, sizeof dist);
+    for (int i = 0; i < n; ++i) dist[i] = map[0][i];
 
     // 选择N-1条边
-    for (int i = 0; i < n - 1; ++i) {
+    for (int i = 1; i < n; ++i) {
 
-        int min = 0x7fffffff;
-        int k = 0;
+        int min = 0x3f3f3f3f, k = -1;
         // 枚举非S所有点，选择最小的边
         for (int j = 1; j < n; ++j) {
-            if (!flag[j] && length[j] < min) {
-                min = length[j];
+            if (!visit[j] && dist[j] < min) {
+                min = dist[j];
                 k = j;
             }
         }
 
+        if (k == -1) {
+            printf("不连通\n");
+            return -1;
+        }
+
         ans += min;
-        cout << "k=" << k << " ,min=" << min << endl;
+        printf("k=%d, min=%d\n", k, min);
         // 将新的点k加入集合S,并通过k更新非S中点的距离
-        flag[k] = true;
+        visit[k] = true;
         for (int j = 1; j < n; ++j) {
-            if (!flag[j] && map[k][j] >= 0 && map[k][j] < length[j]) {
-                length[j] = map[k][j];
+            if (!visit[j] && map[k][j] < dist[j]) {
+                dist[j] = map[k][j];
             }
         }
     }
 
-    cout << "ans=" << ans << endl;
+    return ans;
+}
+
+int main() {
+
+    freopen("../a.in", "r", stdin);
+
+    init();
+    printf("ans=%d\n", prim());
+
     return 0;
 }
