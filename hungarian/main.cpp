f56837b9b274a971ae8822930d83986c7a376f12
kyle.wilson <kyle.wilson@foxmail.com>
2022-10-06 16:33:18 +0800

    optimize

--- hungarian/main.cpp
+++ hungarian/main.cpp
@@ -4,15 +4,14 @@ using namespace std;
 
 #define MAX_M 200
 #define MAX_N 200
-bool map[MAX_N][MAX_M] = {false}, visit[MAX_M];
-int n, m, x[MAX_M], y[MAX_N], ans = 0;
+bool map[MAX_N][MAX_M], visit[MAX_M];
+int n, m, y[MAX_N], ans = 0;
 
 bool hungary(int u) {
     for (int i = 0; i < m; ++i) {
         if (!visit[i] && map[u][i]) {
             visit[i] = true;
             if (y[i] == -1 || hungary(y[i])) {
-                x[u] = i;
                 y[i] = u;
                 return true;
             }
@@ -22,9 +21,8 @@ bool hungary(int u) {
 }
 
 void init() {
-    memset(x, 0xff, MAX_M * 4);
-    memset(y, 0xff, MAX_N * 4);
-    memset(map, false, MAX_N * MAX_M);
+    memset(y, 0xff, sizeof y);
+    memset(map, false, sizeof map);
 
     int num, temp;
     cin >> n >> m;
@@ -38,17 +36,11 @@ void init() {
 }
 
 int main() {
-
     freopen("../a.in", "r", stdin);
-
     init();
     for (int i = 0; i < n; ++i) {
-        if (x[i] == -1) {
-            memset(visit, false, MAX_M);
-            if (hungary(i)) {
-                ans++;
-            }
-        }
+        memset(visit, false, sizeof visit);
+        ans += hungary(i);
     }
     cout << ans << endl;
     return 0;
